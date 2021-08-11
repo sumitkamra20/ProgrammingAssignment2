09aad797f379a45e584811b91f49398dc6b1c64a
Sumit Kamra <sumitkamra@gmail.com>
2021-08-11 14:15:47 +0700

    This commit has both the functions completed as per the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## As per the requirements of the assignment, two functions are written to 
+## create a special Matrix object that can cache its inverse. 
 
-## Write a short comment describing this function
+## the function below creates a special object that caches the matrix and 
+## its inverse. The function returns a list containing functions to set and 
+## retrieve the value of the matrix and its inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv_x <- NULL
+        set <- function(y){
+                x <<- y
+                inv_x <<- NULL
+        }
+        get <- function() x
+        set_inv <- function (y) inv_x <<- y
+        get_inv <- function() inv_x
+        list (set = set, set_inv = set_inv,
+              get = get, get_inv = get_inv)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of the matrix object 
+## created through makeCacheMatrix function and passed as the argument to
+## this function. However, it first checks if the inverse has already been
+## calculated, and if it is, it skips the calculation and simply returns
+## the already calculated inverse. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Check if the inverse is already calculated and if it is, return it 
+        inv_x <- x$get_inv()
+        if(!is.null(inv_x)) {
+                print("Returning cached inverse of the matrix")
+                return(inv_x)
+        }
+        
+        ## This code will only be executed if the inv_x is null (which means it
+        ## doesn't exist)
+        
+        ## compute the inverse of 'x', set its value in cache and return it 
+        inv_x <- solve(x$get())
+        x$set_inv(inv_x)
+        inv_x
+        
 }
